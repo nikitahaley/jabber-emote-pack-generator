a92127537a70a3cfafa3abf154fd691924e26830
nikitahaley <maximumcrisis@gmail.com>
2017-01-26 18:08:08 -0500

    Initial script changes
    Added Pidgin theme generation and a proper readme

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # jabber-emote-pack-generator
 A tool to streamline the creation of emote packs for groups using Pidgin/Adium to communicate.
 
-This obviously doesn't work as intended because I haven't actually done anything yet.
+When executing the script you will be asked for a path; make sure the directory ends in / because this version doesn't check for it and WILL generate empty files with stupid names if given the chance.
+When prompted for a pack logo icon, do not include the path. Pidgin will correctly read just a filename (eg: coolicon.png) assuming that file actually exists in the pack directory.
+
+The current version of this script will generate a Pidgin theme file and Adium plist with the following behavior:
+
+The resulting emotes will be called by their filename without the extension, surrounded by colons (:). For example, Pidgin will be able to call "coolicon.png" with :coolicon: and the theme file will contain this line:
+'''
+! coolicon.png				:coolicon:
+'''
+
+For filenames containing uppercase characters, the resulting emote can be called with sensitive casing or entirely lowercase. For example, Pidgin will be able to call "CoolIcon.png" with :CoolIcon: OR :coolicon: and the theme file will contain this line:
+'''
+! CoolIcon.png				:CoolIcon: :coolicon:
+'''
+
+This is a very basic version of this script and it isn't really recommended to use it, or anything else I've ever touched. Please report bugs in this project's issue tracker.
--- emoteconverter.py
+++ emoteconverter.py
@@ -1,11 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import os
 import sys
 
-with open('theme') as f:
+DIR_EMOTES = str(input("Enter directory to generate theme list for: "))
+INFO_NAME = str(input("Enter name of emote pack: "))
+INFO_DESC = str(input("Enter description of emote pack: "))
+INFO_ICON = str(input("Enter filename of logo icon: "))
+INFO_AUTH = str(input("Enter author name: "))
+DIR_EMOTES_LIST = os.listdir(DIR_EMOTES) # get list of files in dir provided by user
+print("Generating theme list for " + str(len(DIR_EMOTES_LIST)) + " emotes in " + DIR_EMOTES) # print the number of emotes in the directory
+
+f = open(DIR_EMOTES + 'theme', 'w')
+
+f.write("Name=" + INFO_NAME + "\nDescription=" + INFO_DESC + "\nIcon=" + INFO_ICON + "\nAuthor=" + INFO_AUTH + "\n\n[default]\n\n") # tell pidgin about our emote pack
+
+for line in DIR_EMOTES_LIST:
+    EMOTE_LINE = line
+    EMOTE_LINE_NOEXT = EMOTE_LINE.split(".",1)[0] # remove the extension from the current emote file
+    if EMOTE_LINE_NOEXT.islower():  # check for emotes with no uppercase letters so we don't write them twice
+        f.write("! " + EMOTE_LINE + "\t\t\t\t:" + EMOTE_LINE_NOEXT + ":\n")
+    else:
+        f.write("! " + EMOTE_LINE + "\t\t\t\t:" + EMOTE_LINE_NOEXT + ": :" + str(EMOTE_LINE_NOEXT.lower()) + ":\n")
+
+
+f.close()
+
+with open(DIR_EMOTES + 'theme') as f:
     lines = f.readlines()
     f.close()
-
-ff = open('Emoticons.plist', 'w')
+	
+ff = open(DIR_EMOTES + 'Emoticons.plist', 'w')
 
 ff.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
 ff.write("<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n")
@@ -15,7 +39,7 @@ ff.write("<key>Emoticons</key>\n")
 ff.write("<dict>\n")
 
 for line in lines:
-    if not (("=" in line) or line.startswith("#") or line.isspace() or line.startswith("[")): #ignore anything that's not an emote definition
+    if not (("=" in line) or line.startswith("#") or line.isspace() or line.startswith("[")): # ignore anything that's not an emote definition
         words = line.split()
         if (words[0] == "!"):
             words.pop(0) # pop off the exclamation point if there is one
@@ -31,5 +55,5 @@ for line in lines:
         ff.write("</string>\n</dict>\n")
 
 ff.write("</dict>\n<key>AdiumSetVersion</key>\n<integer>1</integer>\n</dict>\n</plist>")
-ff.close
-print ("Emotes file converted to dictionary.")
+ff.close()
+print('Created theme and Emoticons.plist in target directory.')
